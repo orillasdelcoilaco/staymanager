c2dfcf32ce47e04a940f9c6e53c28c1dc1ab15f6
orillasdelcoilaco <orillasdelcoilaco@gmail.com>
2025-10-04 22:31:36 -0300

    estilos v8

--- backend/tailwind.config.js
+++ backend/tailwind.config.js
@@ -4,6 +4,15 @@ module.exports = {
     "./frontend/index.html",
     "./frontend/src/**/*.js",
   ],
+  safelist: [
+    'bg-yellow-500',
+    'bg-orange-500',
+    'bg-red-600',
+    'bg-purple-600',
+    'bg-teal-600',
+    'bg-amber-500',
+    'bg-gray-400',
+  ],
   theme: {
     extend: {},
   },
